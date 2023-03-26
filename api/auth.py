078fbc68fded9c93dae80901c9406b7bf0ec553b
xiangwenbin <tmyyss@163.com>
2023-03-26 08:37:27 +0800

    修改导入语句

--- api/auth.py
+++ api/auth.py
@@ -1,16 +1,24 @@
-from flask import request, jsonify
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
-from werkzeug.security import check_password_hash
 
-from . import api_bp
-from ..models.user import User
+from pop.models.user import User
+from pop.utils.errors import unauthorized, bad_request
 
-@api_bp.route('/auth', methods=['POST'])
-def authenticate():
-    username = request.json.get('username', None)
-    password = request.json.get('password', None)
-    user = User.objects(username=username).first()
-    if not user or not check_password_hash(user.password, password):
-        return jsonify({"msg": "Bad username or password"}), 401
-    access_token = create_access_token(identity=str(user.id))
-    return jsonify({"access_token": access_token}), 200
+bp_auth = Blueprint("auth", __name__, url_prefix="/auth")
+
+@bp_auth.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if not username or not password:
+        return bad_request("Missing username or password")
+
+    user = User.get_by_username(username)
+
+    if user is None or not user.check_password(password):
+        return unauthorized("Invalid username or password")
+
+    access_token = create_access_token(identity=user.id)
+
+    return jsonify(access_token=access_token)
