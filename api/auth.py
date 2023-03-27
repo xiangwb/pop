5d19c43c99070dbf29a82078e6f65a8171016379
xiangwenbin <tmyyss@163.com>
2023-03-27 15:18:14 +0800

    注册蓝图

--- api/auth.py
+++ api/auth.py
@@ -4,9 +4,9 @@ from flask_jwt_extended import create_access_token
 from models.user import User
 from utils.errors import unauthorized, bad_request
 
-bp_auth = Blueprint("auth", __name__, url_prefix="/auth")
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-@bp_auth.route("/login", methods=["POST"])
+@auth_bp.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
