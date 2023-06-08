61d8143df30a27ad9cdcaedd1e11d49d7258f023
xiangwenbin <tmyyss@163.com>
2023-06-08 09:11:17 +0800

    modified:   config/production.py

--- config/production.py
+++ config/production.py
@@ -1,13 +1,4 @@
 from .base import Config
 
 class ProductionConfig(Config):
-    MONGODB_SETTINGS = {
-        'db': 'pop',
-        'host': 'mongodb://mongo:27017',
-        'username': 'root',
-        'password': '1qaz0plm',
-        'authSource': 'admin',
-        'authMechanism': 'SCRAM-SHA-256'
-    }
-    JWT_SECRET_KEY = 'prod_jwt_secret_key'
-    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    DEBUG = False
